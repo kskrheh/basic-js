59de892279c28491bf844fde6c5f57ef1b5f75ad
kskrheh <kskrheh@gmail.com>
2020-11-01 23:08:08 +0300

    feat: basicJS

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let res = '';
+  if (Array.isArray(members)) {
+    let membersStr = members.filter(e => typeof e === 'string');
+    if (membersStr.length === 0) {
+      return false;
+    } else {
+      for (let i = 0; i < membersStr.length; i++) {
+        res += membersStr[i].trim()[0];
+      }
+      return res.toLocaleUpperCase().split('').sort().join('');
+    }
+  } else {
+    return false;
+  }
+    
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let newArr = arr.slice();
+  const moves = ['--discard-next', '--double-next', '--discard-prev', '--double-prev'];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next' && (i + 1) < arr.length) {
+      //console.log('Discard NEXT: ' + arr[i + 1]);
+      i += 1;
+      // continue;
+    } else if (arr[i] === '--double-next' && (i + 1) < arr.length) {
+      //console.log('DOUBLE NEXT: ' + arr[i + 1]);
+      newArr.push(arr[i + 1]); 
+    } else if (arr[i] === '--discard-prev'  && arr[i - 1] !== undefined ) {
+      if (arr[i - 1] === newArr[newArr.length - 1] || (isNaN(arr[i - 1] ) && isNaN(newArr[newArr.length - 1]))) {
+        //console.log('DISCARD PREV: ' + arr[i - 1]);
+        newArr.pop();
+      }
+    } else if (arr[i] === '--double-prev' && arr[i - 1] !== undefined) {
+      if ((arr[i - 1] === newArr[newArr.length - 1] || (isNaN(arr[i - 1] ) && isNaN(newArr[newArr.length - 1]))) && arr[i - 2] !== '--discard-next') {
+        //console.log('DOUBLE PREV: ' + arr[i - 1]);
+        newArr.push(arr[i - 1]);
+      }
+    } else if (!moves.includes(arr[i])) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+
+  
 };
